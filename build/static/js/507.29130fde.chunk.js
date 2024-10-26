f3cefd8558ba330411eafa6ddf95e18c183485c3
mahdirasti2 <mahdi.r@zemtrix.com>
2024-10-26 07:32:55 +0300

    remove build

--- build/static/js/507.29130fde.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkffd_organization_panel=self.webpackChunkffd_organization_panel||[]).push([[507],{1888:(e,n,a)=>{a.r(n),a.d(n,{default:()=>r});a(5043);var i=a(579);function r(){return(0,i.jsx)("div",{children:"RegisterPage"})}}}]);
-//# sourceMappingURL=507.29130fde.chunk.js.map
