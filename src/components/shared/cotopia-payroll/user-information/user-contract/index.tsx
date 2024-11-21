34e9b057f0b5e81dc5c702d2b28a111e452d441c
Youssef9005 <youssefsameh905@gmail.com>
2024-11-21 16:10:20 +0200

    Fix active jobs

--- src/components/shared/cotopia-payroll/user-information/user-contract/index.tsx
+++ src/components/shared/cotopia-payroll/user-information/user-contract/index.tsx
@@ -7,7 +7,7 @@ export default function PayrollUserContract() {
     return (
         <div className="flex-1 bg-white shadow-lg border border-border p-3 rounded-md flex flex-col gap-y-5">
             <PayrollUserInformationHeader title="User Contract" altTitle="Ends in Feb 25 , 2050" />
-            <PayrollActiveJobs />
+            {/* <PayrollActiveJobs /> */}
             <PayrollContractDetails />
         </div>
     )
