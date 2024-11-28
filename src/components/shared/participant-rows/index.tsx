6cc8b8cfbc519b7d37f92371aad2d549bee826c0
Youssef9005 <youssefsameh905@gmail.com>
2024-11-28 14:15:09 +0200

    add mic status to sidebar

--- src/components/shared/participant-rows/index.tsx
+++ src/components/shared/participant-rows/index.tsx
@@ -19,67 +19,65 @@ interface Props {
 
 const ParticipantRows = ({ participants }: Props) => {
   const { room_id } = useRoomContext();
-
+  const { user } = useAuth();
   const { localParticipant } = useLocalParticipant();
 
-  const voiceTrack = localParticipant.getTrackPublication(
-    Track.Source.Microphone
-  );
-
-  const { user } = useAuth();
+  const getParticipantMutedState = (participant: WorkspaceUserType) => {
+    if (participant.username === user?.username) {
+      const voiceTrack = localParticipant.getTrackPublication(
+        Track.Source.Microphone
+      );
+      return voiceTrack?.isMuted ?? true;
+    }
+    return !participant.has_mic;
+  };
 
   if (participants.length === 0) return null;
 
-  const isMuted = voiceTrack?.isMuted ?? true;
-
   return (
-    <div className='w-full flex gap-y-4 flex-col pl-6 pb-5'>
+    <div className="w-full flex gap-y-4 flex-col pl-6 pb-5">
       {participants.map((participant) => {
-        let has_video = participant.has_video;
-        let has_mic = participant.has_mic;
-        let has_screen_share = participant.has_screen_share;
 
-        let accessibilities: ReactNode[] = [];
+        const has_video = participant.has_video;
+        const has_mic = participant.has_mic;
+        const has_screen_share = participant.has_screen_share;
 
-        if (has_video) {
-          accessibilities.push(<VideoIcon size={20} />);
-        }
-
-        if (has_mic) {
-          accessibilities.push(<MicrophoneIcon size={20} />);
-        }
-        if (has_screen_share) {
+        const accessibilities: ReactNode[] = [];
+        if (has_video) accessibilities.push(<VideoIcon size={20} />);
+        if (has_mic) accessibilities.push(<MicrophoneIcon size={20} />);
+        if (has_screen_share)
           accessibilities.push(<MirrorScreenIcon size={20} />);
-        }
 
         const is_mine = participant.username === user?.username;
 
         const userActiveJob = participant.active_job?.title ?? "Idle";
 
+        const isMuted = getParticipantMutedState(participant);
+
         return (
           <div
             key={participant.id}
-            className='flex items-center justify-between w-full'
+            className="flex items-center justify-between w-full"
           >
-            <div className='w-full flex items-center gap-x-2'>
+            <div className="w-full flex items-center gap-x-2">
+
               <ParticipantsWithPopover
-                avatarClss='border border-primary'
-                className='!pb-0'
+                avatarClss="border border-primary"
+                className="!pb-0"
                 roomId={room_id}
                 participants={[participant]}
               />
-              <div className='w-full flex flex-col'>
+              <div className="w-full flex flex-col">
+                <div className="w-full flex items-center justify-between">
 
-                <div className='w-full flex items-center justify-between'>
-
-                  <div className=" flex flex-row items-center gap-x-2">
-                    <span className='font-semibold text-grayscale-paragraph'>
+                  <div className="flex flex-row items-center gap-x-2">
+                    <span className="font-semibold text-grayscale-paragraph">
                       {participant.username}
                     </span>
 
                     {is_mine && (
-                      <div className='flex items-center justify-center p-1 py-[2px] rounded bg-primary-light'>
-                        <span className='text-xs font-medium text-primary-label'>
+                      <div className="flex items-center justify-center p-1 py-[2px] rounded bg-primary-light">
+                        <span className="text-xs font-medium text-primary-label">
                           You
                         </span>
                       </div>
@@ -87,7 +85,7 @@ const ParticipantRows = ({ participants }: Props) => {
                   </div>
 
                   <span className="mr-4 w-6 h-6 bg-red-300/40 rounded-full p-2 text-red-400 flex items-center justify-center">
-                    {isMuted && is_mine ? <MicOff size={20} /> : <Mic size={20} />}
+                    {isMuted ? <MicOff size={24} /> : <Mic size={24} />}
                   </span>
                 </div>
 
@@ -102,7 +100,7 @@ const ParticipantRows = ({ participants }: Props) => {
               </div>
             </div>
 
-            <div className='flex items-center gap-x-1'>{accessibilities}</div>
+            <div className="flex items-center gap-x-1">{accessibilities}</div>
           </div>
         );
       })}
