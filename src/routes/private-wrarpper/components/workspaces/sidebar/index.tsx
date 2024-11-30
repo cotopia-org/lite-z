d051853636ee4541fb5166a2ed8924c6f3dc14a1
katerou22 <xkaterou22@gmail.com>
2024-11-30 14:19:07 +0330

    Fix offline and schedule duplicate and counts.

--- src/routes/private-wrarpper/components/workspaces/sidebar/index.tsx
+++ src/routes/private-wrarpper/components/workspaces/sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import WorkspaceRoomsHolder from "../[slug]/rooms";
 import OfflineUsers from "@/components/shared/room/settings/users/offline";
 import ScheduledUsers from "@/components/shared/room/settings/users/scheduled";
@@ -7,14 +7,10 @@ import {useRoomContext} from "@/components/shared/room/room-context";
 import moment from "moment";
 
 export default function WorkspaceSidebar() {
-  const { workspace_id } = useParams();
+    const {workspace_id} = useParams();
 
 
-
-
-
-
-    const { workspaceUsers, leaderboard, scheduled,room_id,onlineUsers } = useRoomContext();
+    const {workspaceUsers, leaderboard, scheduled, room_id, onlineUsers} = useRoomContext();
 
 
     const today = moment();
@@ -27,27 +23,28 @@ export default function WorkspaceSidebar() {
         .filter((x) => !onlineUserIds.includes(x.user.id));
 
 
-
     const onlines = leaderboard
         ?.filter((x) => x.user.status === "online" && x.user.room_id !== null)
         .map((x) => x.user.id);
 
+
+    const finalSchedulesIds = finalSchedules.map(x => x.user.id)
+
     const allOfflineParticipants = workspaceUsers
         .filter((x) => !onlines.includes(x.id))
-        .filter((x) => !finalSchedules.map(x=>x.id).includes(x.id))
+        .filter((x) => !finalSchedulesIds.includes(x.id))
         .filter((x) => x.last_login !== null)
         .sort((a, b) => moment(b.last_login).unix() - moment(a.last_login).unix());
 
 
-
-  return (
-    <div className='flex flex-col gap-y-4 bg-white h-[calc(100vh-80px)] pb-20 overflow-y-auto relative'>
-      <WorkspaceRoomsHolder workspaceUsers={workspaceUsers} workspace_id={workspace_id as string} />
-      <div className='p-4 flex flex-col gap-y-6'>
-        <ScheduledUsers finalSchedules={finalSchedules}/>
-        <OfflineUsers allOfflineParticipants={allOfflineParticipants} />
-      </div>
-      <WorkspaceActionFab />
-    </div>
-  );
+    return (
+        <div className='flex flex-col gap-y-4 bg-white h-[calc(100vh-80px)] pb-20 overflow-y-auto relative'>
+            <WorkspaceRoomsHolder workspaceUsers={workspaceUsers} workspace_id={workspace_id as string}/>
+            <div className='p-4 flex flex-col gap-y-6'>
+                <ScheduledUsers finalSchedules={finalSchedules}/>
+                <OfflineUsers allOfflineParticipants={allOfflineParticipants}/>
+            </div>
+            <WorkspaceActionFab/>
+        </div>
+    );
 }
