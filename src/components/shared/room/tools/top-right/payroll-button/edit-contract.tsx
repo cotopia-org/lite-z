92500183f17229e11f46657aa20d31a39af0b9f0
mahdirasti2 <mahdi.r@zemtrix.com>
2024-12-10 20:12:05 +0300

    add edit contract

---
+++ src/components/shared/room/tools/top-right/payroll-button/edit-contract.tsx
@@ -0,0 +1,30 @@
+import CotopiaButton from "@/components/shared-ui/c-button";
+import CDialog from "@/components/shared-ui/c-dialog";
+import PayrollCreateContract from "@/pages/cotopia-payroll/admin/create-contract";
+import { UserContractType } from "@/types/contract";
+import { Edit } from "lucide-react";
+
+type Props = {
+  contract: UserContractType;
+};
+
+export default function EditContract({ contract }: Props) {
+  return (
+    <CDialog
+      trigger={(open) => (
+        <CotopiaButton
+          onClick={open}
+          variant={"link"}
+          endIcon={<Edit size={16} />}
+        >
+          Edit
+        </CotopiaButton>
+      )}
+      dialogContentCss='w-[800px] max-w-full max-h-screen md:max-h-[calc(100vh-64px)] overflow-y-auto'
+    >
+      {(close) => (
+        <PayrollCreateContract defaultContract={contract} onBack={close} />
+      )}
+    </CDialog>
+  );
+}
