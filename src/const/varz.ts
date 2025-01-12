c713563c327d3d04fe424297e613d43454da118b
katerou22 <xkaterou22@gmail.com>
2025-01-12 17:21:17 +0330

    Fix domain in invite.

--- src/const/varz.ts
+++ src/const/varz.ts
@@ -1,54 +1,56 @@
 // colors.ts
 export const getCSSVariableValue = (variable: string) => {
-  if (typeof window !== "undefined") {
+  if (typeof window !== 'undefined') {
     return getComputedStyle(document.documentElement)
       .getPropertyValue(variable)
-      .trim()
+      .trim();
   }
-  return ""
-}
+  return '';
+};
 
 export const colors = {
   primary: {
-    default: getCSSVariableValue("--primary"),
-    background: getCSSVariableValue("--primary-background"),
-    primaryForeground: getCSSVariableValue("--primary-foreground"),
-    primaryLabel: getCSSVariableValue("--primary-label"),
-    primaryLight: getCSSVariableValue("--primary-light"),
-    body: getCSSVariableValue("--primary-body"),
+    default: getCSSVariableValue('--primary'),
+    background: getCSSVariableValue('--primary-background'),
+    primaryForeground: getCSSVariableValue('--primary-foreground'),
+    primaryLabel: getCSSVariableValue('--primary-label'),
+    primaryLight: getCSSVariableValue('--primary-light'),
+    body: getCSSVariableValue('--primary-body'),
   },
   grayscale: {
-    grayscaleCaption: getCSSVariableValue("--grayscale-caption"),
-    subtitle: getCSSVariableValue("--grayscale-subtitle"),
+    grayscaleCaption: getCSSVariableValue('--grayscale-caption'),
+    subtitle: getCSSVariableValue('--grayscale-subtitle'),
   },
   secondary: {
-    default: getCSSVariableValue("--secondary"),
-    foreground: getCSSVariableValue("--secondary-foreground"),
+    default: getCSSVariableValue('--secondary'),
+    foreground: getCSSVariableValue('--secondary-foreground'),
   },
-  destructive: getCSSVariableValue("--destructive"),
-  background: getCSSVariableValue("--background"),
-  foreground: getCSSVariableValue("--foreground"),
+  destructive: getCSSVariableValue('--destructive'),
+  background: getCSSVariableValue('--background'),
+  foreground: getCSSVariableValue('--foreground'),
   error: {
-    default: getCSSVariableValue("--error"),
-    "surface-default": getCSSVariableValue("--error-surface-default"),
+    default: getCSSVariableValue('--error'),
+    'surface-default': getCSSVariableValue('--error-surface-default'),
   },
   warning: {
-    default: getCSSVariableValue("--warning"),
-    light: getCSSVariableValue("--warning-light"),
+    default: getCSSVariableValue('--warning'),
+    light: getCSSVariableValue('--warning-light'),
   },
   success: {
-    default: getCSSVariableValue("--success"),
-    "surface-default": getCSSVariableValue("--success-surface-default"),
+    default: getCSSVariableValue('--success'),
+    'surface-default': getCSSVariableValue('--success-surface-default'),
   },
   // Add more as needed
-}
+};
 
 export const VARZ = {
   apiBaseUrl: process.env.REACT_APP_PUBLIC_API_URL,
-  domain: process.env.REACT_APP_PUBLIC_PUBLIC_HOME_URL,
+  domain:
+    process.env.REACT_APP_PUBLIC_PUBLIC_HOME_URL ??
+    'https://lite.cotopia.social',
   serverUrl: process.env.REACT_APP_PUBLIC_LK_SERVER_URL,
-  socketUrl: process.env.REACT_APP_PUBLIC_SOOCKET_URL ?? "",
-  dashboardPage: "/dashboard",
+  socketUrl: process.env.REACT_APP_PUBLIC_SOOCKET_URL ?? '',
+  dashboardPage: '/dashboard',
   loginPage: `/auth/login`,
   registerPage: `/auth/register`,
   forgetPasswordPage: `/auth/forget-password`,
@@ -60,15 +62,15 @@ export const VARZ = {
   jailWidth: 3800,
   jailHeight: 1700,
   sidebarWidth: 376,
-  jailNodeId: "jail-node-custom",
-  jailNodeType: "jailNode",
-  shareScreenNodeType: "shareScreenNode",
-  backgroundNodeType: "bgNode",
+  jailNodeId: 'jail-node-custom',
+  jailNodeType: 'jailNode',
+  shareScreenNodeType: 'shareScreenNode',
+  backgroundNodeType: 'bgNode',
   defaultPerPage: 20,
   pagesLimitDiff: 6,
   defaultPositionOfUserY: 400,
   teleportMargin: 40,
-  userTimeTrackerId: "user-timer-tracker",
+  userTimeTrackerId: 'user-timer-tracker',
   invisibleArea: {
     top: 70,
     bottom: 85,
@@ -78,4 +80,4 @@ export const VARZ = {
     myBulletSize: 48,
     otherBulletSize: 26,
   },
-}
+};
