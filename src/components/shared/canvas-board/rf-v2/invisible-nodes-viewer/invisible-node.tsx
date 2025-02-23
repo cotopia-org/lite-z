b93ae296692fbdd59f9edcca92f384f0b923ad44
mohammadzahedii4713 <mohammadzahedii4713@gmail.com>
2025-02-23 19:28:24 +0330

    add share screen arrow finding

--- src/components/shared/canvas-board/rf-v2/invisible-nodes-viewer/invisible-node.tsx
+++ src/components/shared/canvas-board/rf-v2/invisible-nodes-viewer/invisible-node.tsx
@@ -1,14 +1,14 @@
-import { InvisibleNodeType } from "."
-import { useRoomContext } from "@/components/shared/room/room-context"
-import useAuth from "@/hooks/auth"
-import { VARZ } from "@/const/varz"
-import UserNodeNavigator from "./user-node-navigator"
-import { useReactFlow } from "@xyflow/react"
+import { InvisibleNodeType } from '.';
+import { useRoomContext } from '@/components/shared/room/room-context';
+import useAuth from '@/hooks/auth';
+import { VARZ } from '@/const/varz';
+import UserNodeNavigator from './user-node-navigator';
+import { useReactFlow } from '@xyflow/react';
 
 const InvisibleNode = ({ node }: { node: InvisibleNodeType }) => {
-  const { room } = useRoomContext()
-  const { user: myAccount } = useAuth()
-  const rf = useReactFlow()
+  const { room } = useRoomContext();
+  const { user: myAccount } = useAuth();
+  const rf = useReactFlow();
 
   const {
     node: rfNode,
@@ -18,73 +18,76 @@ const InvisibleNode = ({ node }: { node: InvisibleNodeType }) => {
     delta_y_prime,
     delta_x_prime,
     nodeHeight,
-  } = node
+    nodeWidth,
+  } = node;
 
-  const is_my_node = myAccount.username === rfNode?.id
-  const participants = room?.participants || []
-  const current_user = participants.find((u) => u.username === rfNode.id)
-  const avatar = current_user?.avatar?.url
+  const is_my_node = myAccount.username === rfNode?.id;
+  const participants = room?.participants || [];
+  const current_user = participants.find((u) => u.username === rfNode.id);
+  const avatar = current_user?.avatar?.url;
 
-  if (!delta_x || !delta_y) return null
-  let clss = " absolute z-[2]"
-  let style: { [key: string]: any } = {}
+  if (!delta_x || !delta_y) return null;
+  let clss = ' absolute z-[2]';
+  let style: { [key: string]: any } = {};
 
-  const node_radius = nodeHeight / 2
+  const node_y_axis = nodeHeight / 2;
+  const node_x_axis = nodeWidth / 2;
 
   switch (invisible_side) {
-    case "right":
-      clss += " right-[15px]"
+    case 'right':
+      clss += ' right-[15px]';
       if (delta_y < 0) {
-        clss += " right-[15px] !top-[15px]"
-      } else if (delta_y_prime - nodeHeight / 2 < 0) {
-        clss += " !bottom-[15px] right-[15px]"
+        clss += ' right-[15px] !top-[15px]';
+      } else if (delta_y_prime - node_y_axis < 0) {
+        clss += ' !bottom-[15px] right-[15px]';
       } else
-        style["top"] =
-          `${delta_y + (node_radius - VARZ.invisibleArea.margin)}px`
+        style['top'] =
+          `${delta_y + (node_y_axis - VARZ.invisibleArea.margin)}px`;
 
-      break
-    case "left":
-      clss += " left-[15px]"
-      style["top"] = `${delta_y + node_radius}px`
+      break;
+    case 'left':
+      clss += ' left-[15px]';
+      style['top'] = `${delta_y + node_y_axis}px`;
       if (delta_y < 0) {
-        clss += " !top-[15px] left-[15px]"
+        clss += ' !top-[15px] left-[15px]';
       }
       if (delta_y_prime < 0) {
-        clss += " !bottom-[15px] left-[15px]"
-        style["top"] = "initial"
+        clss += ' !bottom-[15px] left-[15px]';
+        style['top'] = 'initial';
       }
-      break
-    case "top":
-      clss += " top-[15px]"
-      style["left"] = `${delta_x + (node_radius - VARZ.invisibleArea.margin)}px`
+      break;
+    case 'top':
+      clss += ' top-[15px]';
+      style['left'] =
+        `${delta_x + (node_x_axis - VARZ.invisibleArea.margin)}px`;
       if (delta_x < 0) {
-        style["left"] = `${delta_x + nodeHeight}px`
+        style['left'] = `${delta_x + nodeWidth}px`;
       }
-      if (delta_x_prime < -(nodeHeight / 2 - VARZ.invisibleArea.margin)) {
-        style["left"] = `${delta_x - node_radius}px`
+      if (delta_x_prime < -(node_x_axis - VARZ.invisibleArea.margin)) {
+        style['left'] = `${delta_x - node_x_axis}px`;
       }
-      break
-    case "bottom":
-      clss += " bottom-[15px]"
+      break;
+    case 'bottom':
+      clss += ' bottom-[15px]';
       if (delta_x_prime < 0) {
-        clss += " !right-[15px] !bottom-[15px]"
-      } else if (delta_x < -(nodeHeight / 2 - VARZ.invisibleArea.margin)) {
-        clss += " !left-[15px] !bottom-[15px]"
+        clss += ' !right-[15px] !bottom-[15px]';
+      } else if (delta_x < -(node_x_axis - VARZ.invisibleArea.margin)) {
+        clss += ' !left-[15px] !bottom-[15px]';
       } else {
-        style["left"] = `${delta_x + node_radius}px`
+        style['left'] = `${delta_x + node_x_axis}px`;
       }
       if (delta_y_prime < 0) {
-        clss += " !bottom-[15px]"
+        clss += ' !bottom-[15px]';
       }
 
-      break
+      break;
   }
 
   const changeViewportHandler = () => {
-    const x_position = rfNode.position.x
-    const y_position = rfNode.position.y
-    rf.setCenter(x_position, y_position, { zoom: 1.5, duration: 1000 })
-  }
+    const x_position = rfNode.position.x;
+    const y_position = rfNode.position.y;
+    rf.setCenter(x_position, y_position, { zoom: 1.5, duration: 1000 });
+  };
 
   return (
     <div style={style} className={clss}>
@@ -95,7 +98,7 @@ const InvisibleNode = ({ node }: { node: InvisibleNodeType }) => {
         avatar={avatar}
       />
     </div>
-  )
-}
+  );
+};
 
-export default InvisibleNode
+export default InvisibleNode;
--- src/components/shared/canvas-board/rf-v2/invisible-nodes-viewer/user-node-navigator.tsx
+++ src/components/shared/canvas-board/rf-v2/invisible-nodes-viewer/user-node-navigator.tsx
@@ -1,8 +1,9 @@
-import { getNodePositionFromCenter, getRandomColor } from "@/lib/utils";
-import { InvisibleNodeType } from ".";
-import CotopiaAvatar from "@/components/shared-ui/c-avatar";
-import { useMemo } from "react";
-import { useRoomContext } from "@/components/shared/room/room-context";
+import { cn, getNodePositionFromCenter, getRandomColor } from '@/lib/utils';
+import { InvisibleNodeType } from '.';
+import CotopiaAvatar from '@/components/shared-ui/c-avatar';
+import { useMemo } from 'react';
+import { useRoomContext } from '@/components/shared/room/room-context';
+import { VARZ, colors } from '@/const/varz';
 
 type Props = {
   node: InvisibleNodeType;
@@ -11,6 +12,7 @@ type Props = {
   onClick?: () => void;
 };
 
+const SHARE_SCREEN_URL = '/assets/mock/gallery/template/share-screen.png';
 const UserNodeNavigator = ({
   node,
   avatar,
@@ -19,19 +21,23 @@ const UserNodeNavigator = ({
 }: Props) => {
   const { workspaceUsers } = useRoomContext();
 
+  const node_type = node?.node?.type;
+
+  const is_share_screen = node_type === VARZ.shareScreenNodeType;
+
   let nav_clss = `relative rounded-full z-[2]`;
   let avatar_clss =
-    "absolute left-1 top-1 z-[2] text-primary border-primary border cursor-pointer";
-  let arrow_clss = "absolute top-1/2 -translate-y-1/2 rotate-[45deg]";
+    'absolute left-1 top-1 z-[2] text-primary border-primary border cursor-pointer';
+  let arrow_clss = 'absolute top-1/2 -translate-y-1/2 rotate-[45deg]';
 
-  if (isMyNode) {
-    nav_clss += "  w-[48px] h-[48px] ";
-    avatar_clss += " w-[40px] h-[40px]";
-    arrow_clss += " w-6 h-6  right-[-4.5px]";
+  if (isMyNode || is_share_screen) {
+    nav_clss += '  w-[48px] h-[48px] ';
+    avatar_clss += ' w-[40px] h-[40px]';
+    arrow_clss += ' w-6 h-6  right-[-4.5px]';
   } else {
-    nav_clss += "  w-[30px] h-[30px]";
-    avatar_clss += " w-[22px] h-[22px]";
-    arrow_clss += " w-[15px] h-[15px]  right-[-3.7px]";
+    nav_clss += '  w-[30px] h-[30px]';
+    avatar_clss += ' w-[22px] h-[22px]';
+    arrow_clss += ' w-[15px] h-[15px]  right-[-3.7px]';
   }
 
   const { itemPositionX, itemPositionY, coverCenter } = node;
@@ -50,22 +56,27 @@ const UserNodeNavigator = ({
       <div
         style={{
           transform: `rotate(${-degree}deg)`,
-          backgroundColor: getRandomColor(user?.created_at),
+          backgroundColor: is_share_screen
+            ? colors.secondary.default
+            : getRandomColor(user?.created_at),
         }}
         className={nav_clss}
       >
         {/* arrow content */}
         <div
           style={{
-            backgroundColor: getRandomColor(user?.created_at),
+            backgroundColor: is_share_screen
+              ? colors.secondary.default
+              : getRandomColor(user?.created_at),
           }}
           className={arrow_clss}
         ></div>
         {/* arrow content */}
       </div>
+
       <CotopiaAvatar
         onClick={onClick}
-        src={avatar}
+        src={!!is_share_screen ? SHARE_SCREEN_URL : avatar}
         date={user?.created_at}
         className={avatar_clss}
         title={user?.username ? user.name?.[0] : node.node.id[0]}
